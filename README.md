8acfbbe2914315c9e33de4e1fbea087dfe9431db
Miheer Malhotra <miheermalhotra@gmail.com>
2020-08-05 03:43:49 +0530

    Initial commit: UI changes to show the required css grid

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# maersk
+# Maersk UI assignment
+
+This application contains core JavaScript concepts to show the grid items. I have not used any library for passing the information.
+I have used the data from object and parsed it on the html using es6 methods for map
+
+```
+The styling contains css grid styles to display 3 column approach for large screen and single column approach for small screens
+```
+
+#executing the code
+To execute the code simply open the <b><a href="index.html" target="_blank">index.html</a></b> on the browse
---
+++ scripts.js
@@ -0,0 +1,70 @@
+const listElement = [
+  {
+    id: 1,
+    color: "#6F98A8",
+  },
+  {
+    id: 2,
+    color: "#2B8EAD",
+  },
+  {
+    id: 3,
+    color: "#2F454E",
+  },
+  {
+    id: 4,
+    color: "#2B8EAD",
+  },
+  {
+    id: 5,
+    color: "#2F454E",
+  },
+  {
+    id: 6,
+    color: "#BFBFBF",
+  },
+  {
+    id: 7,
+    color: "#BFBFBF",
+  },
+  {
+    id: 8,
+    color: "#6F98A8",
+  },
+  {
+    id: 9,
+    color: "#2F454E",
+  },
+];
+
+function preload(list) {
+  let items = list ? list : listElement;
+  var app = document.querySelector("#app");
+  app.innerHTML =
+    '<ul class="grid-container">' +
+    items
+      .map(function (item) {
+        return (
+          '<li class="grid-item" style=background-color:' +
+          `${item.color}` + ';border-color:' + `${item.color}` +
+          ">" +
+          item.id +
+          "</li>"
+        );
+      })
+      .join("") +
+    "</ul>";
+}
+function sortList() {
+  var list = listElement.sort();
+  this.preload(list);
+}
+
+function shuffleList() {
+  const shuffleList = listElement.slice()
+    for (let i = shuffleList.length - 1; i > 0; i--) {
+        const randomize = Math.floor(Math.random() * (i + 1));
+        [shuffleList[i], shuffleList[randomize]] = [shuffleList[randomize], shuffleList[i]];
+    }
+    this.preload(shuffleList);
+}
